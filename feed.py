b2bc27504e3cbe31ecfd5ad5132d56b4c9e17502
Victor-Jose Lande <36843312+VICTORJOSEARAVENALANDE@users.noreply.github.com>
2025-07-14 12:16:42 +0000

    Converge to portfolio project

--- feed.py
+++ feed.py
@@ -1,52 +1,28 @@
+# feed.py
 import yaml
-import xml.etree.ElementTree as xml_tree
+import xml.etree.ElementTree as ET
 
-# Open feed.yaml doc and call it file. 
-# Load the doc into variable yaml_data.
-# Using yaml.safe_load function to verify that the file loads correctly 
-with open('feed.yaml', 'r') as file:
-    yaml_data = yaml.safe_load(file)
-    
-    # Create the RSS element
-    rss_element = xml_tree.Element('rss', {'version':'2.0',
-    'xmlns:itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd',
-    'xmlns:content':'http://www.purl.org/rss/1.0/modules/content/'
+with open('feed.yaml','r') as f:
+    data = yaml.safe_load(f)
 
-    })
-# Element creating the channel tag
-channel_element = xml_tree.SubElement(rss_element, 'channel')
+rss = ET.Element('rss', {'version':'2.0'})
+ch  = ET.SubElement(rss, 'channel')
 
-link_prefix = yaml_data['link']
+# channel metadata
+ET.SubElement(ch, 'title').text       = data['title']
+ET.SubElement(ch, 'link').text        = data['link']
+ET.SubElement(ch, 'description').text = data['description']
+ET.SubElement(ch, 'language').text    = data['language']
+ET.SubElement(ch, 'image').text       = data['link'] + data['image']
 
-# Inside channel tag, we create title tag, and add text reading from yaml file (in yaml_data)
-xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
-xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
-xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle']
-xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['author']
-xml_tree.SubElement(channel_element, 'description').text = yaml_data['description']
-xml_tree.SubElement(channel_element, 'itunes:image',{'href':link_prefix + yaml_data['image']})
-xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
-xml_tree.SubElement(channel_element, 'link').text = link_prefix
+# each portfolio entry → <item>
+for e in data['entries']:
+    item = ET.SubElement(ch, 'item')
+    ET.SubElement(item, 'title').text       = e['title']
+    ET.SubElement(item, 'link').text        = data['link'] + e['link']
+    ET.SubElement(item, 'description').text = e['description']
+    ET.SubElement(item, 'pubDate').text     = e['date']
 
-xml_tree.SubElement(channel_element, 'itunes:category',{'text': yaml_data['category']})
-
-# Create a loop for every item/episode
-for item in yaml_data['item']:
-    # Create a contained for the item for the episodes
-    item_element = xml_tree.SubElement(channel_element, 'item')
-    xml_tree.SubElement(item_element, 'title').text = item['title']
-    xml_tree.SubElement(item_element, 'itunes:author').text = yaml_data['author'] # No reason for repeating author for each episode since same author
-    xml_tree.SubElement(item_element, 'description').text = item['description']
-    xml_tree.SubElement(item_element, 'itunes:duration').text = item['duration']
-    xml_tree.SubElement(item_element, 'pubDate').text = item['published']
-    
-    # The enclosure, length in bytes for the episode, what type of element, and URL for episode:
-    enclosure = xml_tree.SubElement(item_element, 'enclosure', {
-        'url': link_prefix + item['file'],
-        'type': 'audio/mpeg',
-        'length': item['length']
-    })
-
-# The element we have built will be fed to the output tree so we can send it to a separate file 
-output_tree = xml_tree.ElementTree(rss_element)
-output_tree.write('portfolio.xml',encoding='UTF-8', xml_declaration=True)
+ET.ElementTree(rss).write('portfolio.xml',
+                          encoding='utf-8',
+                          xml_declaration=True)
